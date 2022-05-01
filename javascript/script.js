2b820d094e9e69d46630cf7474d25126cfb52a7d
goobric aka Mikal <goobric@gmail.com>
2022-05-01 16:24:16 +0200

    created js script file and navbar css file

---
+++ javascript/script.js
@@ -0,0 +1,25 @@
+const moreBtn = document.getElementById("main-nav-more");
+const moreNav = document.getElementById("more-nav");
+const crossIcon = document.getElementById("cross-icon");
+
+//Configure Dropdown Visible yes or no
+let extraMenuOpen = false;
+
+moreBtn.addEventListener("click", e => {
+    extraMenuOpen = !extraMenuOpen;
+
+    if (extraMenuOpen){
+        moreBtn.getElementsByClassName("nav-line")[0].style.display = "block";
+        moreNav.style.display = "flex";
+    
+    } else {
+        moreBtn.getElementsByClassName("nav-line")[0].style.display = "none";
+        moreNav.style.display = "none";
+    }
+});
+
+crossIcon.addEventListener("click", e => {
+    extraMenuOpen = false;
+    moreBtn.getElementsByClassName("nav-line")[0].style.display = "none";
+    moreNav.style.display = "none";
+});
